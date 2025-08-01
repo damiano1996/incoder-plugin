04eb7d0b62ae28c4a2095d1635d130c91a430597
Damiano Derin <damiano.derin@gmail.com>
2025-08-01 22:46:14 +0200

    MarkdownEditorPane updated

--- src/main/java/com/github/damiano1996/jetbrains/incoder/ui/components/MarkdownEditorPane.java
+++ src/main/java/com/github/damiano1996/jetbrains/incoder/ui/components/MarkdownEditorPane.java
@@ -2,98 +2,49 @@ package com.github.damiano1996.jetbrains.incoder.ui.components;
 
 import com.intellij.ide.plugins.newui.EmptyCaret;
 import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.util.ui.JBUI;
+import com.intellij.ui.components.JBHtmlPane;
+import com.intellij.ui.components.JBHtmlPaneConfiguration;
+import com.intellij.ui.components.JBHtmlPaneStyleConfiguration;
 import com.vladsch.flexmark.html.HtmlRenderer;
 import com.vladsch.flexmark.parser.Parser;
-import java.awt.*;
-import javax.swing.*;
-import javax.swing.text.html.HTMLEditorKit;
 import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
 
 @Slf4j
-public class MarkdownEditorPane extends JEditorPane {
+public class MarkdownEditorPane extends JBHtmlPane {
 
     private final Parser parser = Parser.builder().build();
     private final HtmlRenderer renderer = HtmlRenderer.builder().build();
-
     private String markdown = "";
 
     public MarkdownEditorPane() {
-        HTMLEditorKit editorKit = new HTMLEditorKit();
-        setEditorKit(editorKit);
-        setContentType("text/html");
+        super(
+                JBHtmlPaneStyleConfiguration.builder()
+                        .enableCodeBlocksBackground(true)
+                        .enableInlineCodeBackground(true)
+                        .build(),
+                JBHtmlPaneConfiguration.builder().build());
+
         setOpaque(false);
-        setDoubleBuffered(true);
         setEditable(false);
-        setHighlighter(null);
-        setEnabled(true);
         setCaret(new EmptyCaret());
+        setContentType("text/html");
     }
 
     @Override
-    public String getText() {
+    public @NotNull String getText() {
         return markdown;
     }
 
     @Override
     public void setText(String text) {
-        markdown = text;
+        this.markdown = text;
 
         ApplicationManager.getApplication()
                 .invokeLater(
                         () -> {
                             String html = renderer.render(parser.parse(markdown));
-
-                            Font font = UIManager.getFont("Label.font");
-                            String fontFamily = font.getFamily();
-                            int fontSize = font.getSize();
-
-                            String styledHtml =
-                                    """
-                                    <html><head><style>
-                                    body {
-                                        font-family: '%s';
-                                        font-size: %dpt;
-                                        background-color: transparent;
-                                        padding: %dpx;
-                                        margin: 0;
-                                        max-width: 400px;
-                                        width: 400px;
-                                    }
-                                    code {
-                                        padding: 2px 4px;
-                                        margin: 1px 2px;
-                                        border-radius: 4px;
-                                        font-family: '%s';
-                                        font-size: %dpt;
-                                    }
-                                    pre {
-                                        padding: %dpx;
-                                        border-radius: 6px;
-                                        overflow: auto;
-                                        font-family: '%s';
-                                        font-size: %dpt;
-                                    }
-                                    pre code {
-                                        background-color: transparent;
-                                        padding: 0;
-                                        margin: 0;
-                                        border: 0;
-                                    }
-                                    </style></head><body>%s</body></html>
-                                    """
-                                            .formatted(
-                                                    fontFamily,
-                                                    fontSize,
-                                                    JBUI.scale(8),
-                                                    fontFamily,
-                                                    fontSize - 1,
-                                                    JBUI.scale(8),
-                                                    fontFamily,
-                                                    fontSize - 1,
-                                                    html);
-
-                            super.setText(styledHtml);
+                            super.setText("<body>" + html + "</body>");
                             revalidate();
                             repaint();
                         });
